05532a41bac15992a7771a6321e152788dd233f5
Kevin He <he.kevin98@gmail.com>
2020-05-16 17:05:03 -0400

    revisions as per martin's request reeeeeeeeeeeeeeeeeeeee

--- commands.js
+++ commands.js
@@ -1,4 +1,5 @@
 var roles = require('./roles.js')
+const minPlayers = 6
 
 module.exports = {
     // Commands
@@ -10,8 +11,8 @@ module.exports = {
     start: function(bot, message) {
         // Starts the game by infecting one random person
         players = roles.playerCount(message.guild, roles.HEALTHY);
-        if (players.length < 6) {
-            message.reply("Not enough players to start the game.");
+        if (players.length < minPlayers) {
+            message.reply("You need at least " + minPlayers + " to begin the game. There are currently " + players.length + " players.");
             return;
         }
         pZero = guild.members.get(players[(Math.random() * players.length)]);
@@ -41,10 +42,11 @@ module.exports = {
         // If a game is already in place, you will be uninfected
         // If a game in not in place and a game starts, you could be patient zero
         if (roles.memberHasRole(message.member, roles.HEALTHY)||roles.memberHasRole(message.member, roles.INFECTED)||roles.memberHasRole(message.member, roles.DEAD)||roles.memberHasRole(message.member, roles.RECOVERED)) {
-            message.reply("You are already part of the game.")
+            message.reply("You are already part of the game.");
             return;
         }
         roles.setRole(message.member, roles.HEALTHY);
+        message.reply("You have joined the game")
         console.log("commands.join()");
     }
 }
