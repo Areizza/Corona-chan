633c802c587432a713c431b28d41e37c1d552278
Priscilla Lo <priscillalo@cmail.carleton.ca>
2020-05-17 16:00:49 -0400

    Fixed issues with roles and missing item group manager

--- ItemGroupManager/itemGroupManager.js
+++ ItemGroupManager/itemGroupManager.js
@@ -8,9 +8,8 @@ class ItemGroupManager {
         // key = userID
         // value = ItemGroup
         this.itemGroups = [];
-        this.playerTotal = roles.playerCount();
-        this.itemList = []
-        this.inventories = {};
+        this.playerTotal = roles.playerCount.length;
+        this.itemList = [];
 
         for (let item in items) {
             this.itemList.push({
@@ -23,13 +22,10 @@ class ItemGroupManager {
         }
 
         //generate some item groups (inventories) and put them into ItemGroups array
-        for (let i = 0; i < this.playerTotal.length; i++) {
+        for (let i = 0; i < this.playerTotal; i++) {
             let inventory = new itemGroup();
             this.itemGroups.push(inventory);            
         }
-
-        //console.log(this.itemGroups);
-        //itemGroups now has a bunch of empty inventories
 
         //calculate the item maximums to integer values based on playerTotal
         //distribute each item (and update maximum -= 1) to a random player
@@ -38,29 +34,16 @@ class ItemGroupManager {
             let name = this.itemList[item].name;
             let maximum = 0;
 
-            if (this.playerTotal.length > 0) {
-                maximum = Math.floor(this.playerTotal.length * this.itemList[item].max);
+            if (this.playerTotal > 0) {
+                maximum = Math.floor(this.playerTotal * this.itemList[item].max);
             }
 
             for (let j = 0; j < maximum; j++) {
                 //get random index for itemGroups to add an item to
-                let index = Math.floor(Math.random() * this.playerTotal.length);
+                let index = Math.floor(Math.random() * this.playerTotal);
                 this.itemGroups[index].inventory[name] += 1;
             }  
         }
-
-        //console.log("new item groups: " + JSON.stringify(this.itemGroups));
-        //assign these itemGroups to people
-        for (let i = 0; i < this.playerTotal.length; i++) {
-
-            //this is supposed to get the player user name somehow?????
-            
-            //this.addItemGroup(this.playerTotal[i], this.itemGroups[i])
-        }
-
-        //console.log(this.inventories);
-
-        //console.log(this.itemList)
     }
 
     addItemGroup(userID, itemGroup) {
--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var commands = require('./commands');
 var infection = require('./infection');
 var roles = require('./roles');
 var game = require('./game')
+var itemGroupManager = require('./ItemGroupManager/itemGroupManager')
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
