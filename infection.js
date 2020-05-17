4a4b799b3173347adc8570ae6e505b6f2971daaf
Martin <mkflui@uwaterloo.ca>
2020-05-16 21:04:28 -0400

    "Basic infection through channel code"

--- infection.js
+++ infection.js
@@ -1,27 +1,52 @@
 var roles = require('./roles.js')
 
+const MAXMESSAGES = 50;
+const TIMELIMIT = 15 * 60;
+
 module.exports = {
 
     // Handle of the possibility of the message author being infected
     handleRisk: function(bot, message) {
-        const member = message.member;
+        
+        // Only healthy people can be infected
+        if (roles.memberHasRole(message.member, roles.HEALTHY)) {
+            const now = new Date().getTime();
+                 
+            message.channel.messages.fetch({ limit: MAXMESSAGES })
+                .then(messages => {
+                    messages = messages.filter(msg => (
+                        ((now - msg.createdTimestamp) < TIMELIMIT) && roles.memberHasRole(msg.member, roles.INFECTED)
+                    ))
+                        
+                    uniqueInfectedUsers = {}        // userID => User
+                    messages.each(msg => {
+                        if (!(msg.author.id in uniqueInfectedUsers)) {
+                            uniqueInfectedUsers[msg.author.id] = msg.author;
+                        }
+                    });
+                    
+                    // TODO: use to calculate rate of infection
+                    for (usr in uniqueInfectedUsers) {
+                        console.log(`${usr.username} is infected`)
+                    }
 
-        if (roles.memberHasRole(member, roles.HEALTHY)) {
-            console.log(member.user.tag + " is " + roles.HEALTHY)
-            const infectionRate = calculateInfectionRate(1, {})
-            if (Math.random() < infectionRate) {
-                roles.setRole(member, roles.INFECTED);
-            }
+                    const infectionRate = calculateInfectionRate()
+                    if (Math.random() < infectionRate) {
+                        roles.setRole(member, roles.INFECTED);
+                        message.reply("You have been infected!")
+                    }
+                })
+                .catch(console.error);
+        } else {
+            console.log(`${message.author.username} is already infected`);
         }
+        return;        
     }
 }
 
-function calculateInfectionRate(numberInfected, items) {
-    // TODO: Add items
-    if (numberInfected == 0) {
-        return 0
-    }
-    return 0.5 * Math.pow(1.1, numberInfected);
+// TODO
+function calculateInfectionRate() {
+    return 0.5;
 }
 
 
